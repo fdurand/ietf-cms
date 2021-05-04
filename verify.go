2d1b0f4ace84ddaea23fe9ea44a61465b0683b96
Durand Fabrice <oeufdure@gmail.com>
2021-05-04 15:14:32 -0400

    Test

--- verify.go
+++ verify.go
@@ -72,57 +72,59 @@ func (sd *SignedData) verify(econtent []byte, opts x509.VerifyOptions) ([][][]*x
 		var signedMessage []byte
 
 		// SignedAttrs is optional if EncapContentInfo eContentType isn't id-data.
-		if si.SignedAttrs == nil {
-			// SignedAttrs may only be absent if EncapContentInfo eContentType is
-			// id-data.
-			if !sd.psd.EncapContentInfo.IsTypeData() {
-				return nil, protocol.ASN1Error{Message: "missing SignedAttrs"}
-			}
+		// if si.SignedAttrs == nil {
+		// 	// SignedAttrs may only be absent if EncapContentInfo eContentType is
+		// 	// id-data.
+		// 	if !sd.psd.EncapContentInfo.IsTypeData() {
+		// 		return nil, protocol.ASN1Error{Message: "missing SignedAttrs"}
+		// 	}
 
-			// If SignedAttrs is absent, the signature is over the original
-			// encapsulated content itself.
-			signedMessage = econtent
-		} else {
-			// If SignedAttrs is present, we validate the mandatory ContentType and
-			// MessageDigest attributes.
-			siContentType, err := si.GetContentTypeAttribute()
-			if err != nil {
-				return nil, err
-			}
-			if !siContentType.Equal(sd.psd.EncapContentInfo.EContentType) {
-				return nil, protocol.ASN1Error{Message: "invalid SignerInfo ContentType attribute"}
-			}
-
-			// Calculate the digest over the actual message.
-			hash, err := si.Hash()
-			if err != nil {
-				return nil, err
-			}
-			actualMessageDigest := hash.New()
-			if _, err = actualMessageDigest.Write(econtent); err != nil {
-				return nil, err
-			}
-
-			// Get the digest from the SignerInfo.
-			messageDigestAttr, err := si.GetMessageDigestAttribute()
-			if err != nil {
-				return nil, err
-			}
-			spew.Dump(messageDigestAttr)
-			spew.Dump(actualMessageDigest.Sum(nil))
-
-			// Make sure message digests match.
-			if !bytes.Equal(messageDigestAttr, actualMessageDigest.Sum(nil)) {
-				return nil, errors.New("invalid message digest")
-			}
-
-			// The signature is over the DER encoded signed attributes, minus the
-			// leading class/tag/length bytes. This includes the digest of the
-			// original message, so it is implicitly signed too.
-			if signedMessage, err = si.SignedAttrs.MarshaledForSigning(); err != nil {
-				return nil, err
-			}
+		// 	// If SignedAttrs is absent, the signature is over the original
+		// 	// encapsulated content itself.
+		// 	signedMessage = econtent
+		// 	spew.Dump("Not signed")
+		// } else {
+		// 	// If SignedAttrs is present, we validate the mandatory ContentType and
+		// MessageDigest attributes.
+		siContentType, err := si.GetContentTypeAttribute()
+		if err != nil {
+			return nil, err
 		}
+		if !siContentType.Equal(sd.psd.EncapContentInfo.EContentType) {
+			return nil, protocol.ASN1Error{Message: "invalid SignerInfo ContentType attribute"}
+		}
+
+		// Calculate the digest over the actual message.
+		hash, err := si.Hash()
+		if err != nil {
+			return nil, err
+		}
+		actualMessageDigest := hash.New()
+		if _, err = actualMessageDigest.Write(econtent); err != nil {
+			return nil, err
+		}
+
+		// Get the digest from the SignerInfo.
+		messageDigestAttr, err := si.GetMessageDigestAttribute()
+		if err != nil {
+			return nil, err
+		}
+
+		spew.Dump(messageDigestAttr)
+		spew.Dump(actualMessageDigest.Sum(nil))
+
+		// Make sure message digests match.
+		if !bytes.Equal(messageDigestAttr, actualMessageDigest.Sum(nil)) {
+			return nil, errors.New("invalid message digest")
+		}
+
+		// The signature is over the DER encoded signed attributes, minus the
+		// leading class/tag/length bytes. This includes the digest of the
+		// original message, so it is implicitly signed too.
+		if signedMessage, err = si.SignedAttrs.MarshaledForSigning(); err != nil {
+			return nil, err
+		}
+		// }
 
 		cert, err := si.FindCertificate(certs)
 		if err != nil {
